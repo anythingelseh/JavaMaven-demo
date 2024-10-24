0e569c0e1c78e1581c295439bf6d5019f7f68358
VishalSinha1103 <vishalsinha1103@gmail.com>
2024-10-24 13:05:15 +0530

    Maven Sample Porject

---
+++ src/main/java/org/sample/mavansample/App.java
@@ -0,0 +1,13 @@
+package org.sample.mavansample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I m in pipeline :)" );
+    }
+}
